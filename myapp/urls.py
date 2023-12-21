f5d46c8144f936b0536d9f54d7ca89fdb6bc2818
jadercaro <jjadercaro192@gmail.com>
2023-12-20 23:21:44 -0500

    update DB and Models

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,8 +5,10 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.hello), 
-    path('about/', views.about)
-
+    path('',views.index), 
+    path('about/', views.about),
+    path('hello/<str:username>',views.hello),
+    path('projects/', views.projects),
+    path('tasks/<str:title>', views.tasks)
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('Hello world')
+def index(request):
+    return HttpResponse("Index page")
+
+def hello(request, username):
+    return HttpResponse('<h2>Hello world %s</h2>' % username)
 
 def about(request):
     return HttpResponse('About')
+
+def projects(requests):                     
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(requests, title):
+    task = Task.objects.get(title=title)
+    ##task= get_object_or_404(Task, id=id)
+    return HttpResponse('tasks: %s' % task.title)
