f9fcb09da81488cd86bc67977add78279d400837
jadercaro <jjadercaro192@gmail.com>
2023-12-31 19:19:56 -0500

    TemplatesFuntional

--- myapp/views.py
+++ myapp/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index page")
+    return render(request,"index.html")
 
 def hello(request, username):
     return HttpResponse('<h2>Hello world... %s</h2>' % username)
 
 def about(request):
-    return HttpResponse('About')
+    return render(request,'about.html')
 
 def projects(requests):                     
     projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+    return render(requests, 'projects.html')
 
-def tasks(requests, title):
-    task = Task.objects.get(title=title)
+def tasks(requests):
+    #task = Task.objects.get(title=title)
     ##task= get_object_or_404(Task, id=id)
-    return HttpResponse('tasks: %s' % task.title)
+    return render(requests, 'tasks.html')
