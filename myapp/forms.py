f4398d330b6195e03b88a70e9779e0cea0073dd5
jadercaro <jjadercaro192@gmail.com>
2024-01-14 18:02:50 -0500

    form widgets

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 
 class createNewTask(forms.Form):
-    title = forms.CharField(label = 'titúlo de tarea', max_length=200)
-    description = forms.CharField(label = 'Descripción de la tarea',widget=forms.Textarea)
+    title = forms.CharField(label = 'titúlo de tarea', max_length=200, widget=forms.TextInput(
+        attrs={"class": "input"}))
+    description = forms.CharField(label = 'Descripción de la tarea',widget=forms.Textarea(attrs={'class': 'input'}))
 
 class createNewProject(forms.Form):
-    name = forms.CharField(label='Nombre del projecto', max_length=200)
+    name = forms.CharField(label='Nombre del projecto', max_length=200, widget=forms.TextInput(attrs={'class':'input'}))
 
     
 
